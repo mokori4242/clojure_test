9bef189dab8cabe1bd7715594e468abee62a56c7
mokori4242 <109648353+mokori4242@users.noreply.github.com>
2024-03-08 09:13:51 +0900

    Chapter 3 (#1)
    * [add]3.3. Arithmetic
    
    * [update]DS_Storeをgit管理から除外
    
    * [add]3章のディレクトリを追加
    
    * [add]3.4:TypeOfNumbers
    
    * [add]3.5:Strings
    
    * [add]3.6:Variables
    
    * [add]3.7:Clojure In File

---
+++ 3_first_steps/5_strings.clj
@@ -0,0 +1,9 @@
+(println "Hello World!")
+(type "Hello")
+(str "1 + 2 is " (+ 1 2))
+(count "Hello")
+(str 1 2 3 4 5 " and so on...")
+(clojure.string/reverse "Hello")
+(clojure.string/upper-case "Hello")
+
+
---
+++ 3_first_steps/6_variables.clj
@@ -0,0 +1,6 @@
+(def my-name "Karthik")
+(println my-name)
+(println "Hello" my-name "!")
+(let [local-variable "moge!!"] (println local-variable))
+(println local-variable)
+
