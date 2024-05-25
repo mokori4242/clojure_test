c8315ffcbe9127684c0b79826b069bcade79c447
mokori4242 <mokori4242@gmail.com>
2024-05-25 12:23:24 +0900

    [add]23.5.Including Eternal Libraries

--- src/test/core.clj
+++ src/test/core.clj
@@ -1,11 +1,28 @@
 (ns test.core
   (:gen-class)
-  (:require [test.decorator :as d]))
+  (:require [test.decorator :as d]
+            [hiccup.core :as h]))
+
+(defn print-to-console [name]
+  (d/print-stars)
+  (println (str "Hello " name "!"))
+  (d/print-stars))
+
+(defn print-to-html [name]
+  (spit "index.html"
+        (h/html
+         [:html
+          [:head
+           [:title "Hello"]]
+          [:body
+           [:h1 (str "Hello " name "!")]
+           [:div {:id "email" :class "selected starred"} "..."]
+           [:div#email.selected.starred "..."]]])))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (d/print-stars)
-  (println (str "Hello " (first args) "!"))
-  (d/print-stars))
+  (if (= "--html" (second args))
+    (print-to-html (first args))
+    (print-to-console (first args))))
 
